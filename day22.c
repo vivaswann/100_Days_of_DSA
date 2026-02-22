51ffa6dc9db621d1acfd44f9e8d01557b557f432
Vivaswan Singh <vivaswan573@gmail.com>
2026-02-22 23:12:03 +0530

    Implement linked list node counting program
    This program counts the number of nodes in a linked list and prints their values along with the count.

---
+++ day22.c
@@ -0,0 +1,57 @@
+/*Problem: Count Nodes in Linked List
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the result
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    for(int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &newNode->data);
+        newNode->next = NULL;
+
+        if(head == NULL)
+            head = temp = newNode;
+        else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    int count = 0;
+    temp = head;
+
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        count++;
+        temp = temp->next;
+    }
+
+    printf("\nCount: %d", count);
+
+    return 0;
+}
