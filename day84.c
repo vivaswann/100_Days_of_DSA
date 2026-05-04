a69d0bdfc121633053da9591f4beea54fffd9a4c
Vivaswan Singh <vivaswan573@gmail.com>
2026-05-04 11:15:22 +0530

    Create day84.c

---
+++ day84.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Read size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read elements
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Insertion Sort
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    // Print sorted array
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
