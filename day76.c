fa02fa035e43ab9b4215a71f547502c0b56d4f4e
Vivaswan Singh <vivaswan573@gmail.com>
2026-05-04 11:12:57 +0530

    Create day76.c

---
+++ day76.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+int n, m;
+
+// DFS function
+void dfs(int node) {
+    visited[node] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int u, v;
+
+    // Input
+    scanf("%d %d", &n, &m);
+
+    // Initialize adjacency matrix and visited array
+    for (int i = 1; i <= n; i++) {
+        visited[i] = 0;
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    // Read edges
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1; // undirected graph
+    }
+
+    int components = 0;
+
+    // Count connected components
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
