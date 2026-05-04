d1fd4e93684852d0b170711e42ac90f23371e6dc
Vivaswan Singh <vivaswan573@gmail.com>
2026-05-04 11:14:06 +0530

    Create day80.c

---
+++ day80.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define INF 1000000000
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int dist[n][n];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+            if (dist[i][j] == -1 && i != j)
+                dist[i][j] = INF;
+        }
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF) {
+                    int through_k = dist[i][k] + dist[k][j];
+                    if (through_k < dist[i][j])
+                        dist[i][j] = through_k;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                printf("-1 ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
